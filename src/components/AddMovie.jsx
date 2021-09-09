42fa4fadde9c5cbdced11d2a2677fe1198a51261
Kristiano <kristianokasper@gmail.com>
2021-09-09 00:51:30 -0300

    Requirement 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -12,12 +12,39 @@ class AddMovie extends Component {
       rating: 0,
       genre: 'action',
     };
+
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler({ target }) {
+    const { name, type } = target;
+    const value = type !== 'checkbox' ? target.value : target.checked;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  inputsGenerator(test, type, name, value) {
+    return (
+      <input
+        data-testid={ test }
+        type={ type }
+        name={ name }
+        value={ value }
+        onChange={ this.changeHandler }
+      />
+    );
   }
 
   render() {
+    const { title } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <input />
+        <label htmlFor="title" data-testid="title-input-label">
+          Título
+          { this.inputsGenerator('title-input', 'text', 'title', title) }
+        </label>
       </form>
     );
   }
