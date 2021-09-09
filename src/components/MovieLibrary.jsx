bfc366b08e44879d174094da5dd9b208d09446ff
Kristiano <kristianokasper@gmail.com>
2021-09-09 03:33:00 -0300

    Requirement 17

--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import AddMovie from './AddMovie';
@@ -8,19 +7,38 @@ class MovieLibrary extends Component {
   constructor(props) {
     super(props);
     const { movies } = this.props;
+
     this.state = {
       searchText: '',
       bookmarkedOnly: false,
       selectedGenre: '',
       movies,
     };
+
+    this.changeHandler = this.changeHandler.bind(this);
+  }
+
+  changeHandler({ target }) {
+    const { name, type } = target;
+    const value = type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
+    const { searchText, selectedGenre, bookmarkedOnly } = this.state;
     return (
       <div>
         <h2> My awesome movie library </h2>
-        <SearchBar />
+        <SearchBar
+          searchText={ searchText }
+          searchTextChange={ this.changeHandler }
+          bookmarkedOnly={ bookmarkedOnly }
+          bookmarkedOnlyChange={ this.changeHandler }
+          selectedGenre={ selectedGenre }
+          selectedGenreChange={ this.changeHandler }
+        />
         <MovieList movies={ this.props.movies } />
         <AddMovie />
       </div>
